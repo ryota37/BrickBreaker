107a4780e46caa279eb6fb3efb73ad4247270dfa
ryota37 <ryota.nakahama@access-company.com>
2024-12-30 10:16:33 +0900

    Separate the Ball class implementation to separate files

---
+++ Ball.cpp
@@ -0,0 +1,54 @@
+﻿#include <Siv3D.hpp>
+#include "Ball.h"
+
+
+double ballRadius;
+Vec2 pos;
+Vec2 velocity;
+
+
+Ball::Ball(double ballRadius, Vec2 initialPos, Vec2 initialVelocity)
+	: ballRadius(ballRadius), pos(initialPos), velocity(initialVelocity)
+{
+
+}
+
+void Ball::setRadius(double ballRadius)
+{
+	ballRadius = ballRadius;
+}
+double Ball::getRadius()
+{
+	return ballRadius;
+}
+
+void Ball::setPos(Vec2 pos)
+{
+	pos = pos;
+}
+Vec2 Ball::getPos()
+{
+	return pos;
+}
+
+void Ball::setVelocity(Vec2 velocity)
+{
+	velocity = velocity;
+}
+Vec2 Ball::getVelocity()
+{
+	return velocity;
+}
+
+void Ball::update()
+{
+	pos += (velocity * Scene::DeltaTime());
+	if ((pos.x <= ballRadius) || (Scene::Width() <= (pos.x + ballRadius)))
+	{
+		velocity.x *= -1.0;
+	}
+	if ((pos.y <= ballRadius) || (Scene::Height() <= (pos.y + ballRadius)))
+	{
+		velocity.y *= -1.0;
+	}
+}
---
+++ Ball.h
@@ -0,0 +1,21 @@
+﻿#include <Siv3D.hpp>
+
+class Ball
+{
+private:
+	double ballRadius;
+	Vec2 pos;
+	Vec2 velocity;
+
+public:
+	Ball(double ballRadius, Vec2 initialPos, Vec2 initialVelocity);
+
+	void setRadius(double ballRadius);
+    double getRadius();
+	void setPos(Vec2 pos);
+    Vec2 getPos();
+	void setVelocity(Vec2 velocity);
+    Vec2 getVelocity();
+
+	void update();
+};
