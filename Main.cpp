50e58e318ed6f474461734dd81f77ba76393c251
ryota37 <ryota.nakahama@access-company.com>
2025-01-05 16:49:59 +0900

    Implement the menu screen and GameOver

--- Main.cpp
+++ Main.cpp
@@ -7,26 +7,77 @@ void Main()
 	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
 
 	Ball ball{ Scene::CenterF().x, Scene::CenterF().y, 20.0,  RandomVec2(500)};
-	Bar bar{ Scene::CenterF().x, Scene::Height() * 0.9, 200.0, 20.0, {300.0,0}};
+	Bar bar{ Scene::CenterF().x, Scene::Height() * 0.9, 200.0, 20.0, {500.0,0}};
+
+	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
+	const Font minifont{ FontMethod::MSDF, 24, Typeface::Bold };
+
+	bool isGameStarted = false;
+	bool isGameOver = false;
+	bool isGameCleared = false;
 
 	while (System::Update())
 	{
-		ball.update();
-		ball.draw();
-		bar.update();
-		bar.draw();
 
-		if ((ball.x <= ball.r) || (Scene::Width() <= (ball.x + ball.r)))
+		if (!isGameStarted)
 		{
-			ball.reflect(true, false);
+			font(U"BrickBreaker").drawAt(Scene::Center().x, Scene::Height() * 5/12);
+			minifont(U"Press Enter to start!!").drawAt(Scene::Center().x, Scene::Height() * 2/3);
+
+			if (SimpleGUI::Button(U"Licenses", Vec2{ Scene::Width() * 1/40, Scene::Height() * 1/40 }))
+			{
+				// ライセンス情報を表示
+				LicenseManager::ShowInBrowser();
+			}
 		}
-		if ((ball.y <= ball.r) || (Scene::Height() <= (ball.y + ball.r)))
+
+		if (KeyEnter.down())
 		{
-			ball.reflect(false, true);
+			isGameStarted = true;
+			ball = { Scene::CenterF().x, Scene::CenterF().y, 20.0,  RandomVec2(500) };
+			bar = { Scene::CenterF().x, Scene::Height() * 0.9, 200.0, 20.0, {500.0,0} };
 		}
-		if (ball.intersects(bar))
+
+		if (isGameStarted && !isGameOver)
 		{
-			ball.reflect(false, true);
+			ball.update();
+			ball.draw();
+			bar.update();
+			bar.draw();
+
+			if ((ball.x <= ball.r) || (Scene::Width() <= (ball.x + ball.r)))
+			{
+				ball.reflect(true, false);
+			}
+			if (ball.y <= ball.r)
+			{
+				ball.reflect(false, true);
+			}
+			if (ball.intersects(bar))
+			{
+				ball.reflect(false, true);
+			}
+
+			if (Scene::Height() <= (ball.y + ball.r))
+			{
+				isGameOver = true;
+			}
 		}
+
+		if (isGameStarted && isGameOver)
+		{
+			font(U"GameOver!").drawAt(Scene::Center());
+		}
+
+		if (isGameStarted)
+		{
+			if (KeySpace.down())
+			{
+				isGameStarted = false;
+				isGameOver = false;
+				isGameCleared = false;
+			}
+		}
+
 	}
 }
